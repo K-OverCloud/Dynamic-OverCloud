f1246e77c30daa6e7c6553e82db170dc1d504c85
Jungsu Han <jshan@smartx.kr>
2018-10-12 22:43:59 +0900

    add telegraf & chronograf

--- workflows/fabric/visible_fabric.sh
+++ workflows/fabric/visible_fabric.sh
@@ -65,3 +65,54 @@ sleep 1
 # Weave 
 ssh -o "StrictHostKeyChecking = no" -i ../../configuration/ssh/$OverCloud_ID.key ubuntu@$post_IP sudo kubectl apply -f dynamic-overcloud/workflows/fabric/weave.yaml
 
+
+
+# install Telegraf & influxdb & chronograf
+
+# download influxdb
+ssh -o "StrictHostKeyChecking = no" -i ../../configuration/ssh/$OverCloud_ID.key ubuntu@$post_IP sudo wget https://dl.influxdata.com/influxdb/releases/influxdb_1.6.3_amd64.deb
+
+# install influxdb
+ssh -o "StrictHostKeyChecking = no" -i ../../configuration/ssh/$OverCloud_ID.key ubuntu@$post_IP sudo dpkg -i influxdb_1.6.3_amd64.deb
+
+# start service
+ssh -o "StrictHostKeyChecking = no" -i ../../configuration/ssh/$OverCloud_ID.key ubuntu@$post_IP sudo service influxdb start
+
+
+# download chronograf
+ssh -o "StrictHostKeyChecking = no" -i ../../configuration/ssh/$OverCloud_ID.key ubuntu@$post_IP sudo wget https://dl.influxdata.com/chronograf/releases/chronograf_1.6.2_amd64.deb
+
+# install chronograf
+ssh -o "StrictHostKeyChecking = no" -i ../../configuration/ssh/$OverCloud_ID.key ubuntu@$post_IP sudo dpkg -i chronograf_1.6.2_amd64.deb
+
+
+
+# telegraf configuration
+
+# find logical cluster IPs
+sql=$(mysql -u overclouds -h $HOST --password=$PASS -e "use overclouds; select IP from logical_cluster where overcloud_ID='$OverCloud_ID';")
+
+for i in $sql; do
+  if [ $i == "IP" ]; then
+    continue
+  fi
+
+
+  # download telegraf
+  ssh -o "StrictHostKeyChecking = no" -i ../configuration/ssh/$OverCloud_ID.key ubuntu@$i sudo wget https://dl.influxdata.com/telegraf/releases/telegraf_1.4.3-1_amd64.deb
+
+  # install telegraf 
+  ssh -o "StrictHostKeyChecking = no" -i ../configuration/ssh/$OverCloud_ID.key ubuntu@$i sudo dpkg -i telegraf_1.4.3-1_amd64.deb
+
+  # configure conf file
+  ssh -o "StrictHostKeyChecking = no" -i ../configuration/ssh/$OverCloud_ID.key ubuntu@$i sudo sed -i "s/http:\/\/localhost/http:\/\/$post_IP/g" /etc/telegraf/telegraf.conf
+
+  # service restart 
+  ssh -o "StrictHostKeyChecking = no" -i ../configuration/ssh/$OverCloud_ID.key ubuntu@$i sudo service telegraf restart
+
+done
+ 
+ 
+
+
+
