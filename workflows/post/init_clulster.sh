c97e5c08260d93947fd895f1b1f226096e48bfd3
Jungsu Han <jshan@smartx.kr>
2019-07-08 15:26:42 +0900

    kubeadm join output fix (1 line of output --> 2 lines of output)

--- workflows/post/init_clulster.sh
+++ workflows/post/init_clulster.sh
@@ -25,7 +25,20 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 
+# It cannot print more than two serialized ouputs
+# For example
+#cat data | grep kubeadm | grep join >> join_data
 
-cat data | grep kubeadm | grep join >> join_data
+# kubeadm join $IP:6443 --token ibrsl1.9n9mvb964xpi3vne \
+
+
+
+# This command prints 2 lines of kubeadm join
+# For example
+# kubeadm join $IP:6443 --token ibrsl1.9n9mvb964xpi3vne \
+#    --discovery-token-ca-cert-hash sha256:862ea2e70a606290398106d87d36e6260f78d298323953d64e3dd99817f9cbf7
+grep -A2 "kubeadm join" data >> join_data
+
+
 #rm data
 
